50804736c57405b43f4a425b1d922758e10ed2eb
Kapil-Kumar-Github <kapilk34537@gmail.com>
2024-07-09 20:59:45 +0530

    third commit

---
+++ src/if_else.java
@@ -0,0 +1,16 @@
+public class if_else {
+    public static void main(String [] args){
+        int a = 23 ;
+        int b = 89 ;
+        if(b>a){
+            System.out.println("b is greater than a.");
+        }
+        else if (a>b){
+            System.out.println("a is greater than b.");
+
+        }
+        else{
+            System.out.println("a nd b both are equal");
+        }
+    }
+}
