774f6cfe2e20a59f1f5ce469be99232dcaa17385
Kapil-Kumar-Github <kapilk34537@gmail.com>
2024-07-09 20:51:21 +0530

    first commit

---
+++ src/If_Program.java
@@ -0,0 +1,12 @@
+public class If_Program {
+    public static void main(String [] args){
+        int a = 23 ;
+        int b = 67 ;
+        if(a<b){
+            System.out.println("b is greater than a.") ;
+
+
+
+        }
+    }
+}
